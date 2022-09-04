700c0400637daee41aee23bb71a554b47ad2c64a
t-ageru13 <tadesseageru13@gmail.com>
2022-09-04 12:13:29 +0300

    upgated

--- RSA/bn_sample.c
+++ RSA/bn_sample.c
@@ -1,31 +1,38 @@
-/* bn_sample.c */
 #include <stdio.h>
 #include <openssl/bn.h>
 #define NBITS 256
+
 void printBN(char *msg, BIGNUM * a)
 {
-/* Use BN_bn2hex(a) for hex string
-* Use BN_bn2dec(a) for decimal string */
 char * number_str = BN_bn2hex(a);
 printf("%s %s\n", msg, number_str);
 OPENSSL_free(number_str);
 }
-int main ()
+
+int main()
 {
-BN_CTX *ctx = BN_CTX_new();
-BIGNUM *a = BN_new();
-BIGNUM *b = BN_new();
-BIGNUM *n = BN_new();
-BIGNUM *res = BN_new();
-// Initialize a, b, n
-BN_generate_prime_ex(a, NBITS, 1, NULL, NULL, NULL);
-BN_dec2bn(&b, "273489463796838501848592769467194369268");
-BN_rand(n, NBITS, 0, 0);
-// res = a*b
-BN_mul(res, a, b, ctx);
-printBN("a * b = ", res);
-// res = aˆb mod n
-BN_mod_exp(res, a, b, n, ctx);
-printBN("aˆc mod n = ", res);
-return 0;
+    BN_CTX *ctx = BN_CTX_new();
+
+    BIGNUM *p = BN_new();
+    BIGNUM *q = BN_new();
+    BIGNUM *e = BN_new();
+    BIGNUM *n = BN_new();
+    BIGNUM *d = BN_new();
+    BIGNUM *res1 = BN_new();
+    BIGNUM *res2 = BN_new();
+    BIGNUM *one = BN_new();
+
+    BN_hex2bn(&p, "F7E75FDC469067FFDC4E847C51F452DF");
+    BN_hex2bn(&q, "E85CED54AF57E53E092113E62F436F4F");
+    BN_hex2bn(&e, "0d88c3");
+    BN_hex2bn(&one, "1");
+
+    BN_sub(res1, p, one);
+    BN_sub(res2, q, one);
+    BN_mul(n, res1, res2, ctx);
+
+    BN_mod_inverse(d, e, n, ctx);
+    printBN("private key d = ", d);
+
+    return 0;
 }
